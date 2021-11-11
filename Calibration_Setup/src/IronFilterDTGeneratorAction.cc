c18e71eabbab8789cfe52e10bf4bfe218b5725ad
pratyush06853 <pratyushkuma@umass.edu>
2021-11-10 21:54:33 -0500

    added geom42 with number of particles

--- Calibration_Setup/src/IronFilterDTGeneratorAction.cc
+++ Calibration_Setup/src/IronFilterDTGeneratorAction.cc
@@ -174,7 +174,8 @@ void IronFilterDTGeneratorAction::GeneratePrimaries(G4Event* anEvent)
   //}
 
   //if (eventID==70398-1) {
-  if (eventID==280565-1) {
+  //if (eventID==280565-1) {
+  if (eventID==174395-1) {
     f->Close();
   }
 
