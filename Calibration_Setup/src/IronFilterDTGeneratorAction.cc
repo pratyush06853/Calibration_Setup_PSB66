539ae9122e8175222d2f0ba8538fec10afbb9cf0
pratyush06853 <pratyushkuma@umass.edu>
2021-11-28 21:49:08 -0500

    added geom48

--- Calibration_Setup/src/IronFilterDTGeneratorAction.cc
+++ Calibration_Setup/src/IronFilterDTGeneratorAction.cc
@@ -130,7 +130,7 @@ void IronFilterDTGeneratorAction::GeneratePrimaries(G4Event* anEvent)
   //cout<<eventID<<endl;
   if(eventID ==0)
   {
-  f = TFile::Open("/home/pp59a/Calibration_Setup/Calibration_Setup_PSB66/Input/CombinedDTFridgelab_geom47.root");
+  f = TFile::Open("/home/pp59a/Calibration_Setup/Calibration_Setup_PSB66/Input/CombinedDTFridgelab_geom48.root");
   //f = TFile::Open("/Volumes/GoogleDrive/My Drive/GraduateWork/Safety_DT/MergedFiles/geom42/CombinedDTFridgelab_geom43.root");
   t1 = (TTree*)f->Get("IronFilter");
   nentries = (Int_t)t1->GetEntries();
@@ -177,7 +177,8 @@ void IronFilterDTGeneratorAction::GeneratePrimaries(G4Event* anEvent)
   //if (eventID==280565-1) {
   //if (eventID==174395-1) {
   //if (eventID==365384-1) {
-  if (eventID==148301-1) {
+  //if (eventID==148301-1) {
+  if (eventID==136922-1) {
     f->Close();
   }
 
